6f77851708471596470b98e3f382cd31ebd6c12b
YehyunLee <glad7cu@gmail.com>
2023-03-30 13:27:48 -0400

    V1

--- part3_investment_simulation.py
+++ part3_investment_simulation.py
@@ -1,3 +1,22 @@
+"""CSC111 Winter 2023 Phase 2: Decoding the Secrets of Successful Stocks (Part 3)
+
+Instructions (READ THIS FIRST!)
+===============================
+
+...
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the personal and private use of students
+taking CSC111 at the University of Toronto St. George campus. All forms of
+distribution of this code, whether as given or with any changes, are
+expressly prohibited. For more information on copyright for CSC111 materials,
+please consult our Course Syllabus.
+
+This file is Copyright (c) 2023 Yehyun Lee, Aung Zwe Maw and Wonjae Lee.
+"""
+
 from datetime import datetime
 from yahoofinancials import YahooFinancials
 
